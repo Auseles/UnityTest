38a66ac308057f5bf708f844a15cfbf25b827a59
Auseles <117265371+Auseles@users.noreply.github.com>
2025-02-27 14:58:23 +0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,8 @@
+let result = 1;
+const intervalID = setInterval(work, 1000);
+ 
+function work() {
+    result = result * 2;
+    console.log("result=", result);
+    if(result >= 32) clearInterval(intervalID);
+}
